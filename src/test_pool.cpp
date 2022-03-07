b294dc188f2dd14d05f46ee2fe0cffb249128b58
gabriel <gabriel.bizzotto@gmail.com>
2022-03-07 00:07:41 -0300

    Revamped pool for efficiency

---
+++ src/test_pool.cpp
@@ -0,0 +1,172 @@
+
+#include <stdlib.h>
+#include <vector>
+#include <chrono>
+#include <set>
+
+#include <utttil/pool.hpp>
+#include <utttil/assert.hpp>
+
+bool test_basics()
+{
+	utttil::fixed_pool<int> pool(1024);
+
+	ASSERT_ACT(pool.size(), ==, 0, return false);
+	ASSERT_ACT(pool.empty(), ==, true, return false);
+	ASSERT_ACT(pool.full(), ==, false, return false);
+
+	int * a = pool.alloc();
+
+	ASSERT_ACT(a, !=, nullptr, return false);
+	ASSERT_ACT(pool.size(), ==, 1, return false);
+	ASSERT_ACT(pool.empty(), ==, false, return false);
+	ASSERT_ACT(pool.full(), ==, false, return false);
+
+	*a = 1234;
+
+	pool.free(a);
+
+	ASSERT_ACT(pool.size(), ==, 0, return false);
+	ASSERT_ACT(pool.empty(), ==, true, return false);
+	ASSERT_ACT(pool.full(), ==, false, return false);
+
+	return true;
+}
+
+bool test_full()
+{
+	utttil::fixed_pool<int> pool(1024);
+
+	int *ptrs[1024];
+
+	for (int i=0 ; i<1024 ; i++)
+	{
+		ptrs[i] = pool.alloc();
+		*ptrs[i] = 1000000 + i;
+	}
+
+	ASSERT_ACT(pool.size(), ==, 1024, return false);
+	ASSERT_ACT(pool.empty(), ==, false, return false);
+	ASSERT_ACT(pool.full(), ==, true,  return false);
+
+	for (int i=0 ; i<1024 ; i++)
+	{
+		ASSERT_MSG_ACT(*ptrs[i], ==, 1000000 + i, std::to_string(i), return false);
+		pool.free(ptrs[i]);
+	}
+
+	ASSERT_ACT(pool.size(), ==, 0, return false);
+	ASSERT_ACT(pool.empty(), ==, true, return false);
+	ASSERT_ACT(pool.full(), ==, false,  return false);
+
+	return true;
+}
+
+bool test_holes()
+{
+	utttil::fixed_pool<int> pool(1024);
+
+	std::vector<int*> ptrs;
+	std::vector<int> values;
+
+	for (int i=0 ; i<1024 ; i++)
+	{
+		ptrs.push_back(pool.alloc());
+		*ptrs.back() = 1000000 + i;
+		values.push_back(1000000 + i);
+	}
+
+	ASSERT_ACT(pool.size(), ==, 1024, return false);
+	ASSERT_ACT(pool.empty(), ==, false, return false);
+	ASSERT_ACT(pool.full(), ==, true,  return false);
+	for (size_t i=0 ; i<ptrs.size() ; i++)
+		ASSERT_MSG_ACT(*ptrs[i], ==, values[i], std::to_string(i), return false);
+
+	for (int i=0 ; i<128 ; i++)
+	{
+		int idx = rand() % ptrs.size();
+		int * ptr = ptrs[idx];
+		ptrs.erase(ptrs.begin()+idx);
+		values.erase(values.begin()+idx);
+		pool.free(ptr);
+	}
+
+	ASSERT_ACT(pool.size(), ==, 1024-128, return false);
+	ASSERT_ACT(pool.empty(), ==, false, return false);
+	ASSERT_ACT(pool.full(), ==, false,  return false);
+	for (size_t i=0 ; i<ptrs.size() ; i++)
+		ASSERT_MSG_ACT(*ptrs[i], ==, values[i], std::to_string(i), return false);
+
+	for (int i=0 ; i<128 ; i++)
+	{
+		ptrs.push_back(pool.alloc());
+		*ptrs.back() = 1000000 + i;
+		values.push_back(1000000 + i);
+	}
+
+	ASSERT_ACT(pool.size(), ==, 1024, return false);
+	ASSERT_ACT(pool.empty(), ==, false, return false);
+	ASSERT_ACT(pool.full(), ==, true,  return false);
+	for (size_t i=0 ; i<ptrs.size() ; i++)
+		ASSERT_MSG_ACT(*ptrs[i], ==, values[i], std::to_string(i), return false);
+
+	return true;
+}
+
+bool test_fuzz()
+{
+	auto seed = time(NULL); // 1646605269;
+	std::cout << "Seed: " << seed << std::endl;
+	srand(seed);
+
+	utttil::fixed_pool<int> pool(1024*1024);
+	std::set<int*> ptrs;
+
+	for (int i=0 ; i<1024 ; i++)
+	{
+		int * ptr = pool.alloc();
+		ASSERT_ACT(ptrs.find(ptr) == ptrs.end(), ==, true, return false);
+		ptrs.insert(ptr);
+	}
+
+	size_t insert_count = 0;
+	size_t delete_count = 0;
+
+	for ( auto deadline = std::chrono::steady_clock::now() + std::chrono::seconds(10)
+		; std::chrono::steady_clock::now() < deadline
+		; )
+	{
+		int n = rand() % ptrs.size();
+		delete_count += n;
+		for (int i=0 ; i<n ; i++)
+		{
+			auto it = std::next(ptrs.begin(), ((rand() % ptrs.size())));
+			pool.free(*it);
+			ptrs.erase(it);
+		}		
+		int m = rand() % ptrs.size()+100;
+		insert_count += m;
+		for (int i=0 ; i<m ; i++)
+		{
+			int * ptr = pool.alloc();
+			ASSERT_MSG_ACT(ptrs.find(ptr) == ptrs.end(), ==, true, std::to_string(i), return false);
+			ptrs.insert(ptr);
+		}
+	}
+
+	std::cout << insert_count << " inserts, " << delete_count << " deletes." << std::endl;
+
+	return true;
+}
+
+int main()
+{
+	bool success = true
+		&& test_basics()
+		&& test_full()
+		&& test_holes()
+		&& test_fuzz()
+		;
+
+	return success ? 0 : 1;
+}
--- utttil/pool.hpp
+++ utttil/pool.hpp
@@ -1,32 +1,70 @@
 
 #pragma once
 
+#include <memory>
+#include <type_traits>
+#include <limits>
+#include <exception>
+
 namespace utttil {
 
-template<typename T>
-struct object_pool
+template<typename T, typename Index=std::uint32_t>
+struct fixed_pool
 {
-	std::deque<T> collection;
-	std::deque<size_t> free_idx;
+	union Obj
+	{
+		T t;
+		Index next_index;
+	};
+
+	std::unique_ptr<Obj[]> collection;
+	Index first_free;
+	Index size_;
+	const Index capacity_;
+
+	fixed_pool(size_t size)
+		: collection(std::make_unique<Obj[]>(size))
+		, first_free(0)
+		, size_(0)
+		, capacity_(size)
+	{
+		if ( ! collection)
+			throw std::bad_alloc();
+		if (std::numeric_limits<Index>::max() < size)
+			throw std::invalid_argument("pool dynamic size too big for static index type");
+		for (Index i=0 ; i<size-1 ; ++i)
+			collection[i].next_index = i+1;
+	}
+
+	size_t size() const { return size_; }
+	size_t capacity() const { return capacity_; }
+	bool empty() const { return size_ == 0; }
+	bool full() const { return size_ == capacity_; }
 
 	T* alloc()
 	{
-		if ( ! free_idx.empty())
-		{
-			T *ptr = &collection[free_idx.back()];
-			free_idx.pop_back();
-			return ptr;
-		}
-		collection.emplace_back();
-		collection.back().mempool_idx = collection.size()-1;
-		return & collection.back();
+		if (full())
+			return nullptr;
+
+		T * result = &collection[first_free].t;
+		first_free = collection[first_free].next_index;
+		++size_;
+
+		return result;
 	}
 
-	void free(T *ptr)
+	void free(T * ptr)
 	{
-		size_t idx = ptr->mempool_idx;
-		free_idx.push_back(idx);
+		Index idx = ((Obj*)ptr) - &collection[0];
+
+		collection[idx].next_index = first_free;
+		first_free = idx;
+
+		--size_;
 	}
 };
+
+template<typename T> using fixed_pool_tiny  = fixed_pool<T,std::uint8_t>;
+template<typename T> using fixed_pool_large = fixed_pool<T,std::uint64_t>;
 
 } // namespace
