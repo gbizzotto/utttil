67813b0abf31e2bc117e737e0322bb75f0305099
gabriel <gabriel.bizzotto@gmail.com>
2022-08-08 09:14:25 -0300

    Fix fixed_pool so that it can hold non-PODs.

--- src/test_pool.cpp
+++ src/test_pool.cpp
@@ -113,60 +113,138 @@ bool test_holes()
 	return true;
 }
 
+bool test_contains()
+{
+	utttil::fixed_pool<int> pool1(16);
+	utttil::fixed_pool<int> pool2(1024);
+
+	int * p1_i1 = pool1.alloc();
+	int * p1_i2 = pool1.alloc();
+
+	int * p2_i1 = pool2.alloc();
+	int * p2_i2 = pool2.alloc();
+
+	ASSERT_ACT(pool1.contains(p1_i1), ==, true, return false);
+	ASSERT_ACT(pool1.contains(p1_i2), ==, true, return false);
+	ASSERT_ACT(pool1.contains(p2_i1), ==, false, return false);
+	ASSERT_ACT(pool1.contains(p2_i2), ==, false, return false);
+
+	ASSERT_ACT(pool2.contains(p2_i1), ==, true, return false);
+	ASSERT_ACT(pool2.contains(p2_i2), ==, true, return false);
+	ASSERT_ACT(pool2.contains(p1_i1), ==, false, return false);
+	ASSERT_ACT(pool2.contains(p1_i2), ==, false, return false);
+
+	return true;
+}
+
+bool test_index_of()
+{
+	utttil::fixed_pool<int> pool1(16);
+	utttil::fixed_pool<int> pool2(1024);
+
+	int * p1_i1 = pool1.alloc();
+	int * p1_i2 = pool1.alloc();
+
+	int * p2_i1 = pool2.alloc();
+	int * p2_i2 = pool2.alloc();
+
+	ASSERT_ACT(pool1.index_of(p1_i1), ==, 0u, return false);
+	ASSERT_ACT(pool1.index_of(p1_i2), ==, 1u, return false);
+	ASSERT_ACT(&pool1.element_at(0), ==, p1_i1, return false);
+	ASSERT_ACT(&pool1.element_at(1), ==, p1_i2, return false);
+
+	ASSERT_ACT(pool2.index_of(p2_i1), ==, 0u, return false);
+	ASSERT_ACT(pool2.index_of(p2_i2), ==, 1u, return false);
+	ASSERT_ACT(&pool2.element_at(0), ==, p2_i1, return false);
+	ASSERT_ACT(&pool2.element_at(1), ==, p2_i2, return false);
+
+	pool1.free(p1_i1);
+	int * p1_i3 = pool1.alloc();
+	ASSERT_ACT(pool1.index_of(p1_i3), ==, 0u, return false);
+	ASSERT_ACT(&pool1.element_at(0), ==, p1_i3, return false);
+	ASSERT_ACT(&pool1.element_at(1), ==, p1_i2, return false);
+
+	pool2.free(p2_i1);
+	int * p2_i3 = pool2.alloc();
+	ASSERT_ACT(pool2.index_of(p2_i3), ==, 0u, return false);
+	ASSERT_ACT(&pool2.element_at(0), ==, p2_i3, return false);
+	ASSERT_ACT(&pool2.element_at(1), ==, p2_i2, return false);
+
+	return true;
+}
+
 bool test_fuzz()
 {
-	auto seed = time(NULL); // 1646605269;
-	std::cout << "Seed: " << seed << std::endl;
-	srand(seed);
-
-	utttil::fixed_pool<int> pool(1024*1024);
-	std::set<int*> ptrs;
-
-	for (int i=0 ; i<1024 ; i++)
+	auto x = std::make_shared<int>(123);
 	{
-		int * ptr = pool.alloc();
-		ASSERT_ACT(ptrs.find(ptr) == ptrs.end(), ==, true, return false);
-		ptrs.insert(ptr);
-	}
+		using T = decltype(x);
+		utttil::fixed_pool<T> pool(1024*1024);
+		std::set<T*> ptrs;
 
-	size_t insert_count = 0;
-	size_t delete_count = 0;
-
-	for ( auto deadline = std::chrono::steady_clock::now() + std::chrono::seconds(3)
-		; std::chrono::steady_clock::now() < deadline
-		; )
-	{
-		int n = rand() % ptrs.size();
-		delete_count += n;
-		for (int i=0 ; i<n ; i++)
+		for (int i=0 ; i<1024 ; i++)
 		{
-			auto it = std::next(ptrs.begin(), ((rand() % ptrs.size())));
-			pool.free(*it);
-			ptrs.erase(it);
-		}		
-		int m = rand() % ptrs.size()+100;
-		insert_count += m;
-		for (int i=0 ; i<m ; i++)
-		{
-			int * ptr = pool.alloc();
-			ASSERT_MSG_ACT(ptrs.find(ptr) == ptrs.end(), ==, true, std::to_string(i), return false);
+			T * ptr = pool.alloc(x);
+			ASSERT_ACT(ptrs.find(ptr) == ptrs.end(), ==, true, return false);
 			ptrs.insert(ptr);
 		}
-	}
 
-	std::cout << insert_count << " inserts, " << delete_count << " deletes." << std::endl;
+		size_t insert_count = 0;
+		size_t delete_count = 0;
+
+		for ( auto deadline = std::chrono::steady_clock::now() + std::chrono::seconds(3)
+			; std::chrono::steady_clock::now() < deadline
+			; )
+		{
+			int n = rand() % ptrs.size();
+			delete_count += n;
+			for (int i=0 ; i<n ; i++)
+			{
+				auto it = std::next(ptrs.begin(), ((rand() % ptrs.size())));
+				pool.free(*it);
+				ptrs.erase(it);
+			}		
+			int m = rand() % ptrs.size()+100;
+			insert_count += m;
+			for (int i=0 ; i<m ; i++)
+			{
+				T * ptr = pool.alloc(x);
+				ASSERT_MSG_ACT(ptrs.find(ptr) == ptrs.end(), ==, true, std::to_string(i), return false);
+				ptrs.insert(ptr);
+			}
+		}
+
+		ASSERT_ACT(pool.size(), ==, (size_t)x.use_count()-1, return false);
+
+		size_t for_each_count = 0;
+		pool.for_each([&](T &){ for_each_count++; });
+		ASSERT_ACT(for_each_count, ==, pool.size(), return false);
+
+		std::cout << insert_count << " inserts, " << delete_count << " deletes." << std::endl;
+	}
+	ASSERT_ACT(0, ==, x.use_count()-1, return false);
 
 	return true;
 }
 
 int main()
 {
+	auto seed = time(NULL); // 1646605269;
+	std::cout << "Seed: " << seed << std::endl;
+	srand(seed);
+
 	bool success = true
 		&& test_basics()
 		&& test_full()
 		&& test_holes()
+		&& test_contains()
+		&& test_index_of()
 		&& test_fuzz()
 		;
+
+	if (success)
+		std::cout << "Success" << std::endl;
+	else
+		std::cout << "Failure" << std::endl;
 
 	return success ? 0 : 1;
 }
--- utttil/pool.hpp
+++ utttil/pool.hpp
@@ -11,29 +11,36 @@ namespace utttil {
 template<typename T, typename Index=std::uint32_t>
 struct fixed_pool
 {
+	using index_t = Index;
+
 	union Obj
 	{
 		T t;
 		Index next_index;
 	};
 
-	std::unique_ptr<Obj[]> collection;
-	Index first_free;
-	Index size_;
-	const Index capacity_;
+	Obj * collection;
+	index_t first_free;
+	index_t size_;
+	const index_t capacity_;
 
-	fixed_pool(size_t size)
-		: collection(std::make_unique<Obj[]>(size))
+	fixed_pool(size_t capacity)
+		: collection((Obj*)malloc(sizeof(Obj) * capacity))
 		, first_free(0)
 		, size_(0)
-		, capacity_(size)
+		, capacity_(capacity)
 	{
 		if ( ! collection)
 			throw std::bad_alloc();
-		if (std::numeric_limits<Index>::max() < size)
+		if (std::numeric_limits<index_t>::max() < capacity)
 			throw std::invalid_argument("pool dynamic size too big for static index type");
-		for (Index i=0 ; i<size-1 ; ++i)
+		for (index_t i=0 ; i<capacity-1 ; ++i)
 			collection[i].next_index = i+1;
+	}
+	~fixed_pool()
+	{
+		for_each([](T & t){ t.~T(); });
+		::free(collection);
 	}
 
 	size_t size() const { return size_; }
@@ -50,16 +57,77 @@ struct fixed_pool
 		first_free = collection[first_free].next_index;
 		++size_;
 
+		new (result) T;
+		return result;
+	}
+	T* alloc(const T & other)
+	{
+		if (full())
+			return nullptr;
+
+		T * result = &collection[first_free].t;
+		first_free = collection[first_free].next_index;
+		++size_;
+
+		new (result) T(other);
+		return result;
+	}
+	T* alloc(T && other)
+	{
+		if (full())
+			return nullptr;
+
+		T * result = &collection[first_free].t;
+		first_free = collection[first_free].next_index;
+		++size_;
+
+		new (result) T(std::move(other));
 		return result;
 	}
 
 	void free(T * ptr)
 	{
-		Index idx = ((Obj*)ptr) - &collection[0];
+		index_t idx = index_of(ptr);
 
+		ptr->~T();
 		collection[idx].next_index = first_free;
 		first_free = idx;
 		--size_;
+	}
+
+	bool contains(T * ptr)
+	{
+		return ptr >= &collection[0].t && ptr < &collection[capacity_].t;
+	}
+	index_t index_of(T * ptr) { return ((Obj*) ptr) - &collection[0]; }
+	index_t index_of(T & ptr) { return ((Obj*)&ptr) - &collection[0]; }
+	const T & element_at(index_t idx) const
+	{
+		return collection[idx].t;
+	}
+	T & element_at(index_t idx)
+	{
+		return collection[idx].t;
+	}
+
+	template<typename F>
+	void for_each(F f)
+	{
+		std::vector<bool> constructed(capacity(), true);
+		size_t free_cells = capacity() - size();
+		for ( index_t idx = first_free
+			; free_cells-- != 0
+			; idx = collection[idx].next_index)
+		{
+			constructed[idx] = false;
+		}
+		for ( index_t idx = 0
+			; idx < capacity()
+			; idx++ )
+		{
+			if (constructed[idx])
+				f(collection[idx].t);
+		}
 	}
 };
 
