07aa1c02ab51a632f19de503c9d4908ba443ce98
gabriel <gabriel.bizzotto@gmail.com>
2022-08-22 17:25:01 -0300

    New seqdict_vector.

--- src/test_dict.cpp
+++ src/test_dict.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <immintrin.h>
 
 #include <utttil/assert.hpp>
 
@@ -45,9 +46,12 @@ bool test_seqdict_pool_string()
 
 	seqdict_pool p(1024);
 
+	ASSERT_ACT(p.next_key(), ==, 0, return false);
+
 	auto [seq0, inserted0] = p.push_back(std::string("100"));
 	auto [seq1, inserted1] = p.push_back("101");
 
+	ASSERT_ACT(p.next_key(), ==, 2, return false);
 	ASSERT_ACT(inserted0, ==, true, return false);
 	ASSERT_ACT(inserted1, ==, true, return false);
 	ASSERT_ACT(seq0, ==, typename seqdict_pool::seq_type(0), return false);
@@ -73,11 +77,48 @@ bool test_seqdict_pool_string()
 	return true;
 }
 
+bool test_seqdict_vector()
+{
+	using seqdict_vector = utttil::seqdict_vector<uint64_t, std::string>;
+
+	seqdict_vector s(16);
+
+	ASSERT_ACT(s.next_key(), ==,seqdict_vector::seq_type(0), return false);
+
+	auto [seq0, inserted0] = s.push_back(std::string("100"));
+	auto [seq1, inserted1] = s.push_back("101");
+
+	ASSERT_ACT(s.next_key(), ==, seqdict_vector::seq_type(2), return false);
+	ASSERT_ACT(inserted0, ==, true, return false);
+	ASSERT_ACT(inserted1, ==, true, return false);
+	ASSERT_ACT(seq0, ==, typename seqdict_vector::seq_type(0), return false);
+	ASSERT_ACT(seq1, ==, typename seqdict_vector::seq_type(1), return false);
+	ASSERT_ACT(s.size(), ==, size_t(2), return false);
+	ASSERT_ACT(s.find(seq0), !=, nullptr, return false);
+	ASSERT_ACT(s.find(seq1), !=, nullptr, return false);
+	ASSERT_ACT(*s.find(seq0), ==, "100", return false);
+	ASSERT_ACT(*s.find(seq1), ==, "101", return false);
+
+	for (int i=s.next_key() ; i<1025 ; i++)
+	{
+		ASSERT_ACT(s.size(), ==, (size_t)i, return false);
+		auto [seq, inserted] = s.push_back(std::to_string(i));
+		ASSERT_ACT(seq, ==, (size_t)i, return false);
+		ASSERT_ACT(s.find(seq), !=, nullptr, return false);
+		ASSERT_ACT(*s.find(seq), ==, std::to_string(i), return false);
+	}
+
+	std::cout << s.capacity() << std::endl;
+
+	return true;
+}
+
 int main()
 {
 	bool success = true
 		&& test_seqdict_pool()
 		&& test_seqdict_pool_string()
+		&& test_seqdict_vector()
 		;
 
 	if (success)
--- utttil/dict.hpp
+++ utttil/dict.hpp
@@ -1,6 +1,11 @@
 
 #pragma once
 
+#include <thread>
+#include <vector>
+
+#include <boost/container/flat_map.hpp>
+
 #include "absl/container/flat_hash_map.h"
 #include "unique_int.hpp"
 #include "pool.hpp"
@@ -79,6 +84,7 @@ struct seqdict_pool
 
 	seq_type next_key() const { return next_seq; }
 	size_t size() const { return pool.size(); }
+	size_t capacity() const { return pool.capacity(); }
 	
 	template<typename...Args>
 	std::tuple<seq_type,inserted_t> push_back(Args...args)
@@ -116,6 +122,100 @@ struct seqdict_pool
 };
 
 template<typename K, typename V>
+struct seqdict_vector
+{
+	using SelfType = seqdict_vector<K,V>;
+	using   seq_type = K;
+	using value_type = V;
+	using inserted_t = bool;
+
+	enum resize_state_t
+	{
+		None,
+		Resizing,
+		Done,
+	};
+
+	std::vector<V> data;
+	std::vector<V> new_data;
+	std::atomic_int resize_state;
+
+	seqdict_vector(uint32_t size)
+		: resize_state(None)
+	{
+		data.reserve(size);
+	}
+
+	seq_type next_key() const { return data.size(); }
+	size_t size() const { return data.size(); }
+	size_t capacity() const { return data.capacity(); }
+
+	V & get(const K & k)
+	{
+		if (k >= data.size())
+			data.resize(k+1);
+		return data[k];
+	}
+	template<typename T, typename Tag>       V & get (const unique_int<T,Tag> & i)       { return get (i.value()); }
+	template<typename T, typename Tag> const V * find(const unique_int<T,Tag> & i) const { return find(i.value()); }
+	template<typename T, typename Tag>       V * find(const unique_int<T,Tag> & i)       { return find(i.value()); }
+
+	auto   begin() { return data.begin(); }
+	auto     end() { return data.  end(); }
+	auto &  back() { return data. back(); }
+	void reserve(size_t s) { data.reserve(s); }
+
+	template<typename...Args>
+	std::tuple<seq_type,inserted_t> push_back(Args...args)
+	{
+		if (size() > capacity()*0.75 && resize_state == None)
+		{
+			resize_state = Resizing;
+			std::thread([&]() {
+					new_data.reserve(data.capacity() * 2);
+					for (auto & v : data)
+						new_data.push_back(v);
+					for(auto it=data.begin()+new_data.size() ; it!=data.end() ; ++it)
+						new_data.push_back(*it);
+					this->resize_state = Done;
+				}).detach();
+		}
+		if(resize_state == Resizing)
+			std::cout << "lets wait" << std::endl;
+		while(resize_state == Resizing)
+		{}
+		if (resize_state == Done)
+		{
+			for(auto it=data.begin()+new_data.size() ; it!=data.end() ; ++it)
+				new_data.push_back(std::move(*it));
+			std::swap(data, new_data);
+			new_data.clear();
+			resize_state = None;
+		}
+		data.emplace_back(args...);
+		return {K(data.size()-1), true};
+	}
+
+	bool contains(K k)
+	{
+		return k >= 0 && k < size();
+	}
+	V * find(K k)
+	{
+		if ( ! contains(k))
+			return nullptr;
+		return &data[k];
+	}
+
+	template<typename F>
+	void for_all(F f)
+	{
+		for (size_t k=1 ; k<data.size() ; ++k)
+			f((K)k, data[k]);
+	}
+};
+
+template<typename K, typename V>
 struct AbslPtrDict : public absl::flat_hash_map<K,V*>
 {
 	using SelfType = AbslPtrDict;
@@ -146,6 +246,39 @@ struct AbslDict : public absl::flat_hash_map<K,V>
 {
 	using SelfType = AbslDict;
 	using Super = absl::flat_hash_map<K,V>;
+
+	V & get(const K & k)
+	{
+		auto emplace_pair = this->try_emplace(k, V());
+		return emplace_pair.first->second;
+	}
+	const V * find(const K & k) const
+	{
+		auto it = Super::find(k);
+		if (it == Super::end())
+			return nullptr;
+		return &it->second;
+	}
+	V * find(const K & k)
+	{
+		return const_cast<V*>(const_cast<const SelfType*>(this)->find(k));
+	}
+	std::tuple<V&,bool> try_insert(const K & k, const V & v)
+	{
+		auto emplace_pair = this->try_emplace(k, v);
+		return {emplace_pair.first->second, emplace_pair.second};
+	}
+
+	template<typename T, typename Tag>       V & get (const unique_int<T,Tag> & i)       { return get (i.value()); }
+	template<typename T, typename Tag> const V * find(const unique_int<T,Tag> & i) const { return find(i.value()); }
+	template<typename T, typename Tag>       V * find(const unique_int<T,Tag> & i)       { return find(i.value()); }
+};
+
+template<typename K, typename V>
+struct BoostDict : public boost::container::flat_map<K,V>
+{
+	using SelfType = BoostDict;
+	using Super = boost::container::flat_map<K,V>;
 
 	V & get(const K & k)
 	{
