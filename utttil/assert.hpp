0538c98b3c6a24b9bb495e0768e82ed0dc3609b4
gabriel <gabriel.bizzotto@gmail.com>
2022-12-28 09:33:59 -0300

    More legible asserts messages.

--- utttil/assert.hpp
+++ utttil/assert.hpp
@@ -2,12 +2,12 @@
 #pragma once
 
 #include <iostream>
+#include "utttil/timestamp.hpp"
 
-#define ASSERT_(       left, op, right             ) { auto l=(left); auto r=(right); if (!(l op (r))) { std::cerr << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r                << std::endl;         } }
-#define ASSERT_ACT(    left, op, right,      action) { auto l=(left); auto r=(right); if (!(l op (r))) { std::cerr << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r                << std::endl; action; } }
-#define ASSERT_MSG(    left, op, right, msg        ) { auto l=(left); auto r=(right); if (!(l op (r))) { std::cerr << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r << ", " << msg << std::endl;         } }
-#define ASSERT_MSG_ACT(left, op, right, msg, action) { auto l=(left); auto r=(right); if (!(l op (r))) { std::cerr << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r << ", " << msg << std::endl; action; } }
-
-#define ASSERTNOT_ACT(    left, op, right,      action) { auto l=(left); auto r=(right); if (l op (r)) { std::cerr << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r                << std::endl; action; } }
-#define ASSERTNOT_MSG(    left, op, right, msg        ) { auto l=(left); auto r=(right); if (l op (r)) { std::cerr << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r << ", " << msg << std::endl;         } }
-#define ASSERTNOT_MSG_ACT(left, op, right, msg, action) { auto l=(left); auto r=(right); if (l op (r)) { std::cerr << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r << ", " << msg << std::endl; action; } }
+#define ASSERT_(       left, op, right             ) { auto l=(left); auto r=(right); if (!(l op (r))) { std::cerr << utttil::UTCTimestampISO8601() << " ----------ASSERT FAILED " << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r                << std::endl;         } }
+#define ASSERT_ACT(    left, op, right,      action) { auto l=(left); auto r=(right); if (!(l op (r))) { std::cerr << utttil::UTCTimestampISO8601() << " ----------ASSERT FAILED " << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r                << std::endl; action; } }
+#define ASSERT_MSG(    left, op, right, msg        ) { auto l=(left); auto r=(right); if (!(l op (r))) { std::cerr << utttil::UTCTimestampISO8601() << " ----------ASSERT FAILED " << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r << ", " << msg << std::endl;         } }
+#define ASSERT_MSG_ACT(left, op, right, msg, action) { auto l=(left); auto r=(right); if (!(l op (r))) { std::cerr << utttil::UTCTimestampISO8601() << " ----------ASSERT FAILED " << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r << ", " << msg << std::endl; action; } }
+#define ASSERTNOT_ACT(    left, op, right,      action) { auto l=(left); auto r=(right); if (l op (r)) { std::cerr << utttil::UTCTimestampISO8601() << " ----------ASSERT FAILED " << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r                << std::endl; action; } }
+#define ASSERTNOT_MSG(    left, op, right, msg        ) { auto l=(left); auto r=(right); if (l op (r)) { std::cerr << utttil::UTCTimestampISO8601() << " ----------ASSERT FAILED " << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r << ", " << msg << std::endl;         } }
+#define ASSERTNOT_MSG_ACT(left, op, right, msg, action) { auto l=(left); auto r=(right); if (l op (r)) { std::cerr << utttil::UTCTimestampISO8601() << " ----------ASSERT FAILED " << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r << ", " << msg << std::endl; action; } }
