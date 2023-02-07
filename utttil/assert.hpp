5e4fa592d1b5184ea93ca322039c8dc84035ab4c
gabriel <gabriel.bizzotto@gmail.com>
2023-02-07 08:03:55 -0300

    New ASSERT_ACT_QUIET.

--- utttil/assert.hpp
+++ utttil/assert.hpp
@@ -4,8 +4,9 @@
 #include <iostream>
 #include "utttil/timestamp.hpp"
 
-#define ASSERT_(       left, op, right             ) { auto l=(left); auto r=(right); if (!(l op (r))) { std::cerr << utttil::UTCTimestampISO8601() << " ----------ASSERT FAILED " << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r                << std::endl;         } }
-#define ASSERT_ACT(    left, op, right,      action) { auto l=(left); auto r=(right); if (!(l op (r))) { std::cerr << utttil::UTCTimestampISO8601() << " ----------ASSERT FAILED " << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r                << std::endl; action; } }
+#define ASSERT_(         left, op, right             ) { auto l=(left); auto r=(right); if (!(l op (r))) { std::cerr << utttil::UTCTimestampISO8601() << " ----------ASSERT FAILED " << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r                << std::endl;         } }
+#define ASSERT_ACT(      left, op, right,      action) { auto l=(left); auto r=(right); if (!(l op (r))) { std::cerr << utttil::UTCTimestampISO8601() << " ----------ASSERT FAILED " << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r                << std::endl; action; } }
+#define ASSERT_ACT_QUIET(left, op, right,      action) { if (!((left) op (right))) { action; } }
 #define ASSERT_MSG(    left, op, right, msg        ) { auto l=(left); auto r=(right); if (!(l op (r))) { std::cerr << utttil::UTCTimestampISO8601() << " ----------ASSERT FAILED " << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r << ", " << msg << std::endl;         } }
 #define ASSERT_MSG_ACT(left, op, right, msg, action) { auto l=(left); auto r=(right); if (!(l op (r))) { std::cerr << utttil::UTCTimestampISO8601() << " ----------ASSERT FAILED " << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r << ", " << msg << std::endl; action; } }
 #define ASSERTNOT_ACT(    left, op, right,      action) { auto l=(left); auto r=(right); if (l op (r)) { std::cerr << utttil::UTCTimestampISO8601() << " ----------ASSERT FAILED " << __FILE__ << ":" << __LINE__ << " : " << l << ' ' << #op << ' ' << r                << std::endl; action; } }
